5eb54d8569ba36dd283b7953a845f06351648d4b
Alex <alex171274@rambler.ru>
2020-01-16 11:33:45 +0300

    Initial Commit

---
+++ iTunesPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesPlayer
+//
+//  Created by Alex on 16.01.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
